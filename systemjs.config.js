b0d88364c63c357bcedb796fbba6e3eca132ff41
dinony <dinonysaur@gmail.com>
2017-05-11 10:47:01 +0200

    Add missing maps to animation bundles in systemjs config

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,11 +6,14 @@ SystemJS.config({
     ts: 'npm:plugin-typescript/lib',
     typescript: 'npm:typescript',
 
+    '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
+    '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
     '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
     '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
     '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
     '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
     '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
     '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
     '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
     '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
@@ -33,7 +36,7 @@ SystemJS.config({
     src: {
       main: 'main.ts',
       defaultExtension: 'ts',
-      meta: {'*.ts': {loader:'ts'}}  
+      meta: {'*.ts': {loader:'ts'}}
     },
     rxjs: {
       defaultExtension: 'js'
